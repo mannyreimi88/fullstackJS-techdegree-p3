be27e85f2e2e9704909e0fec790e1600bf29f33f
Manny <manny@holdex.io>
2020-11-23 14:58:39 +0800

    updated script.js
    added "Color" dropdown functionality

--- js/script.js
+++ js/script.js
@@ -6,20 +6,46 @@ nameInput.focus();
 // "Job Role" section
     // "Other job role?" text field input is hidden by default on page load
 const otherJobRoleInput = document.querySelector('#other-job-role');
-otherJobRoleInput.style.display = 'none';
+otherJobRoleInput.hidden = true;
 
     // "Other job role?" text field appears when the 'Other' option is selected on the "Job Role" dropdown, hides otherwise.
 const jobRoleDropdown = document.querySelector('#title');
 jobRoleDropdown.addEventListener('change', e => {
     const option = e.target.value
     if(option === 'other'){
-        otherJobRoleInput.style.display = 'inherit';
+        otherJobRoleInput.hidden = false;
     } else {
-        otherJobRoleInput.style.display = 'none';
+        otherJobRoleInput.hidden = true;
     }
 });
 
 // "T-Shirt Info" section
-    // "Color" dropdown is disabled on page load.
+const designDropdown = document.querySelector('#design');
 const colorDropdown = document.querySelector('#color');
-colorDropdown.disabled = true;
+
+    // "Color" dropdown is disabled on page load.
+colorDropdown.disabled = true;;
+
+    // "Color" dropdown adjusts to user actions on "Design" dropdown
+designDropdown.addEventListener('change', e => {
+    const option = e.target.value;
+    const colorOptions = colorDropdown.children;
+
+        // "Color" dropdown is enabled once a "Design" dropdown option is selected
+    colorDropdown.disabled = false;
+
+        // "Color" dropdown adjust to one of the two options selected on "Design" dropdown:
+            // Only "Color" options from the relevant "Design" theme are available for selection, others are hidden.
+            // One of the available "Color" options is "pre-selected" on the "Color" dropdown
+
+    for(let i=0; i<colorOptions.length; i++){
+        const theme = colorOptions[i].getAttribute('data-theme');
+        if (option === theme){
+            colorOptions[i].hidden = false;
+            colorOptions[i].setAttribute('selected', '');
+        } else {
+            colorOptions[i].hidden = true;
+            colorOptions[i].removeAttribute('selected');
+        }
+    }
+});
